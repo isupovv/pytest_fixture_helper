36f1d1b8d00225962ba648168470a4d4ad8f915b
Vladislav Isupov <isupovvi95@gmail.com>
2023-01-25 23:29:44 -0500

    add move to fixture into one file

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -16,7 +18,41 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('pytest-fixture-helper.moveToFixture', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Conftest helper!');
+
+		if (!vscode.workspace.workspaceFolders) {
+			return vscode.window.showErrorMessage('Please open a project folder first');
+		}
+	
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			return;
+		}
+	
+		const language = editor.document.languageId;
+		if (language !== 'python') {
+			return;
+		}
+		
+		const selection = editor.selection;
+		const text = editor.document.getText(selection);
+		vscode.window.showInformationMessage(text);
+
+		const lineCount = editor.document.lineCount;
+
+		for (let i=0; i < lineCount; i++) {
+			const line = editor.document.lineAt(i);
+			if (line.text.includes(`def ${text}`)) {
+				let range = editor.document.lineAt(i).range;
+				editor.selection =  new vscode.Selection(range.start, range.end);
+				editor.revealRange(range);
+			}
+		}
+	
+		const folderPath = vscode.workspace.workspaceFolders[0].uri
+			.toString()
+			.split(':')[1];
+		
+		vscode.window.showInformationMessage('Path is ' + folderPath);
 	});
 
 	context.subscriptions.push(disposable);
