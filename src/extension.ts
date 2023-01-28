10b0dfb869747ba6babf8dc6832334c066ea996e
Vladislav Isupov <soup.ae@gmail.com>
2023-01-28 15:51:39 -0500

    remove comment

--- src/extension.ts
+++ src/extension.ts
@@ -66,9 +66,6 @@ export function activate(context: vscode.ExtensionContext) {
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('pytest-fixture-helper.moveToFixture', async () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-
 		if (!vscode.workspace.workspaceFolders) {
 			return vscode.window.showErrorMessage('Please open a project folder first');
 		}
